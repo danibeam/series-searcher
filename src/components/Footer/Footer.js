9d5ce6d68b01a0ad15234c175dd4310ae1f2c55b
daniel.belmonte <d.belmonte.amoros@accenture.com>
2019-06-05 18:11:29 +0200

    Added horizontal bar separator

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Footer.css';
+import { Divider } from 'antd';
 
 function Footer() {
     return (
         <div className="Footer">
+            <Divider dashed />
             <p>
                 Series searcher - Powered by <a href="http://www.omdbapi.com" target="blank">OMDb</a> v1.0
             </p>
